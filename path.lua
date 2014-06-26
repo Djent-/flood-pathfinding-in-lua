8c2683601196bafeef385aa582779be6772d5495
Djent- <inflames2361@aim.com>
2014-06-26 19:50:13 -0400

    fixed error in flood table initialization. created a table print function for debug purposes

--- path.lua
+++ path.lua
@@ -4,15 +4,15 @@
 --0 for passable, 1 for impassable
 
 function path(t, startx, starty, endx, endy)
-	if type(t) ~= "table" then error("Provide a table.")
-	if type(t[1]) ~= "table" then error("Provide a table of tables.")
+	if type(t) ~= "table" then error("Provide a table.") end
+	if type(t[1]) ~= "table" then error("Provide a table of tables.") end
 
 	local y, x = #t, #t[1]
 	local _t = {}
 	for _y = 1, y do
 		_t[_y] = {}
 		for _x = 1, x do
-			if t[y][x] == 0 then
+			if t[_y][_x] == 0 then
 				_t[_y][_x] = x * y --passable
 			else
 				_t[_y][_x] = 0 --impassable
@@ -20,9 +20,10 @@ function path(t, startx, starty, endx, endy)
 		end
 	end
 	flood(_t, startx, starty, 1)
+	testprinttable(_t)
 end
 
-local function flood(t, x, y, n)
+function flood(t, x, y, n)
 	if not t[y] then return end
 	if not t[y][x] then return end
 
@@ -40,3 +41,28 @@ local function flood(t, x, y, n)
 	flood(t, x - 1, y + 1, n + 1) --bottom left
 	flood(t, x + 1, y + 1, n + 1) --bottom right
 end
+
+function testprinttable(t)
+	if not t then return end
+	if not t[1] then return end
+	if not t[1][1] then return end
+
+	local output
+	for y = 1, #t do
+		output = ""
+		for x = 1, #t[y] do
+			output = output .. t[y][x]
+		end
+		print(output)
+	end
+end
+
+a = {
+{0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0},
+{0,0,0,0,1,0,0,0,0},
+{0,0,0,0,1,0,0,0,0},
+{0,0,0,0,1,0,0,0,0}
+}
+
+path(a, 1, 5, 8, 5)
