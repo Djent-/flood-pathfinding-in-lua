9997d10da9ae225cbe3bf647dd97e7dd4c664009
Djent- <inflames2361@aim.com>
2014-06-26 19:17:41 -0400

    initialized flood table, added flood()

--- path.lua
+++ path.lua
@@ -10,8 +10,19 @@ function path(t, startx, starty, endx, endy)
 	local y, x = #t, #t[1]
 	local _t = {}
 	for _y = 1, y do
+		_t[_y] = {}
 		for _x = 1, x do
-
+			if t[y][x] == 0 then
+				_t[_y][_x] = 0
+			else
+				_t[_y][_x] = x * y + 1
+			end
 		end
 	end
+	_t[endy][endx] = x * y
+	flood(_t, startx, starty)
+end
+
+function flood(t, x, y)
+
 end
