fae2c3d2b979ef0ce1480e1230d00fdede824f3c
Djent- <inflames2361@aim.com>
2014-06-26 19:09:30 -0400

    Added start and end points to arguments

---
+++ path.lua
@@ -0,0 +1,15 @@
+--path.lua
+--2014.6.26
+
+function path(t, startx, starty, endx, endy)
+	if type(t) ~= "table" then error("Provide a table.")
+	if type(t[1]) ~= "table" then error("Provide a table of tables.")
+
+	local y, x = #t, #t[1]
+	local _t = {}
+	for _y = 1, y do
+		for _x = 1, x do
+
+		end
+	end
+end
